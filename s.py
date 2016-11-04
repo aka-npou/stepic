0cd9ec60d893cd47f952cd9bb298f5b01f1bd7d8
aka-npou <npou@mail.ru>
2016-11-04 22:43:24 +0300

    test
    test

---
+++ s.py
@@ -0,0 +1,15 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 1234))
+s.listen(10)
+n=0
+while True:
+	conn, addr = s.accept()
+	while True:
+		data = conn.recv(1024)
+		n=n+1
+		if not data: break
+		if data == 'close':break
+		conn.send(data)
+	conn.close()
+	if n == 11:break
